e85d8288860f062766e5780c3039788d3a20f35e
sjilani443 <143477393+sjilani443@users.noreply.github.com>
2024-04-07 08:40:18 +0530

    Update indexx.php

--- indexx.php
+++ indexx.php
@@ -104,6 +104,58 @@
 
                     </div>
                 </div>
+
+                <div class="foryou">
+                    <p class="poppins-light">Our Recommendations</p>
+                    <div class="cardcontainer">
+                        <form id="hoverIndexForm" method="GET" action="indexx.php">
+                            <input type="hidden" name="hoverIndex" id="hoverIndexInput">
+                        </form>
+                        <?php
+                        include("db.php");
+                        $sql = "SELECT * FROM album_main2";
+                        $res = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($res) > 0) {
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                echo "<div class='card'>";
+                                echo "<img src='$row[Album_Cover]' alt='' />";
+                                echo "<p>$row[Album_Name] </p>";
+                                echo "<p class='index'>$row[i] </p>";
+                                echo "<div class='icon'>";
+                                echo "<i class='fa-solid fa-play'></i> </div>          
+                        </div>";
+                            }
+                        }
+                        ?>
+                    </div>
+                </div>
+
+                <div class="foryou ">
+                    <p class="poppins-light">Try These</p>
+                    <div class="cardcontainer">
+                        <form id="hoverIndexForm" method="GET" action="indexx.php">
+                            <input type="hidden" name="hoverIndex" id="hoverIndexInput">
+                        </form>
+                        <?php
+                        include("db.php");
+                        $sql = "SELECT * FROM album_main1";
+                        $res = mysqli_query($conn, $sql);
+                        if (mysqli_num_rows($res) > 0) {
+                            while ($row = mysqli_fetch_assoc($res)) {
+                                echo "<div class='card'>";
+                                echo "<img src='$row[Album_Cover]' alt='' />";
+                                echo "<p>$row[Album_Name] </p>";
+                                echo "<p class='index'>$row[i] </p>";
+                                echo "<div class='icon'>";
+                                echo "<i class='fa-solid fa-play'></i> </div>          
+                        </div>";
+                            }
+                        }
+                        ?>
+
+                    </div>
+                </div>
+
             </div>
 
 
