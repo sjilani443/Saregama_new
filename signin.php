239dd3a6b228c35b6e728bdb099fac661507470d
sjilani443 <143477393+sjilani443@users.noreply.github.com>
2024-05-02 23:14:58 +0530

    css signin

--- signin.php
+++ signin.php
@@ -28,74 +28,32 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sign In</title>
-    <style>
-      * {
-        margin: 0;
-        padding: 0;
-      }
-      body {
-        font-size: large;
-        background-color: lavender;
-        display: flex;
-        flex-direction: column;
-      }
-      form {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        gap: 20px;
-      }
-      form div {
-        display: flex;
-        flex-direction: column;
-        font-size: larger;
-        gap: 10px;
-      }
-      form div button{
-        font-size: large;
-      }
-      form input{
-        height: 20px;
-      }
-      fieldset {
-        margin: 10% 30% 0 30%;
-        padding: 50px;
-        gap: 20px;
-      }
-      fieldset div{
-        display: flex;
-        justify-content: center;
-      }
-      fieldset h2{
-        text-align: center;
-        margin: 5% 0 5% 0;
-      }
-    </style>
+    <link rel="stylesheet" href="signcss.css">
   </head>
   <body>
-    <fieldset>
-      <h2>Sign In</h2>
-      <?php
-      if($isvalid):?>
-      <em>Invalid details</em>
-      <?php endif; ?>
-      <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+  <div class="container">
+    <fieldset class="login-box">
+        <h2>Sign In</h2>
+        <?php if($isvalid): ?>
+        <em>Invalid details</em>
+        <?php endif; ?>
+        <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post">
+            <div>
+                <label for="email">Email </label>
+                <input type="email" id="email" name="email" value="<?= htmlspecialchars($_POST["email"]?? "")?>" />
+            </div>
+            <div>
+                <label for="password">Password </label>
+                <input type="password" id="password" name="pass" />
+            </div>
+            <div>
+                <button>Sign In</button>
+            </div>
+        </form>
         <div>
-          <label for="email">Email </label>
-          <input type="email" id="email" name="email" value="<?= htmlspecialchars($_POST["email"]?? "")?>" />
+            Don't have an account? <a href="signup.php">Sign Up</a>
         </div>
-        <div>
-          <label for="password">Password </label>
-          <input type="password" id="password" name="pass" />
-        </div>
-        <div>
-          <button>Sign In</button>
-        </div>
-      </form>
-      <div>
-        Don't have an account? <a href="signup.php">Sign Up</a>
-      </div>
     </fieldset>
+    </div>
   </body>
 </html>
